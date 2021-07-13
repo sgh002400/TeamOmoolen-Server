4b813711b28fe3e94955dc8ce1277e6db28b0b23
anzun0 <zxzimin@naver.com>
2021-07-13 16:45:52 +0900

    [Refactor] 지환 홈뷰, 검색 API refactoring

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Query, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './common/decorators/user.decorator';
 import { FilterConditionDto } from './common/dto/filter.condition.dto';
-import { saveOnBoardingDataDto } from './common/dto/save.onboarding.data.dto';
 import { SearchQueryStringDto } from './common/dto/search.querystring.dto';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ObjectId = require('mongodb').ObjectID;
@@ -10,31 +10,24 @@ const ObjectId = require('mongodb').ObjectID;
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post('/saveOnBoardingData')
-  async saveOnBoardingData(@Body() body: saveOnBoardingDataDto) {
-  
-    const id = new ObjectId("60e7975bb8cadc72adc44f33");
-    return this.appService.saveOnBoardingData(body, id);
-  }
-  
   @Get('/home')
-  async getHomeData() {
-    const id = new ObjectId("60e7975bb8cadc72adc44f33");
-    return this.appService.findHomeData(id);
+  async getHomeData(@User() user) {
+    const userId = ObjectId(user.userId);
+    return this.appService.findHomeData(userId);
   }
-  
+
   @Get('/searchWindow')
   async getPopularItem() {
     return this.appService.findPopularItem();
   }
-  
+
   @Get('/searchProduct')
-  async getSearchProduct(@Query() query: SearchQueryStringDto) {
-    return this.appService.getSearchProduct(query);
+  async getSearchProduct(@Query() query) {
+    return this.appService.getSearchProduct(query.keyword, query.page, query.sort, query.order);
   }
-  
+
   @Get('/getFilteredList')
-  async filteredList(@Body() body: FilterConditionDto) {
-    return this.appService.getFilteredList(body);
+  async filteredList(@Body() body: FilterConditionDto, @Query() query) {
+    return this.appService.getFilteredList(body, query.page, query.sort, query.order);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../entities/Users';
 import { ObjectID, Repository } from 'typeorm';
+import { saveOnBoardingDataDto } from '../common/dto/save.onboarding.data.dto';
+import { WantedLensDto } from '../common/dto/wantedLens.dto';
+import { SuitedLensDto } from '../common/dto/suitedLens.dto';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const mongodb = require('mongodb');
 
@@ -30,5 +33,28 @@ export class UsersService {
     return this.usersRepository.findOne({
       where: { oauthKey: oauthKey },
     });
+  }
+
+  async saveOnBoardingData(body: saveOnBoardingDataDto, id: ObjectID) {
+    const findUser = await this.findUserById(id);
+
+    findUser.age = body.age;
+    findUser.gender = body.gender;
+    findUser.wearTime = body.wearTime;
+
+    const wantedLens = new WantedLensDto();
+    wantedLens.category = body.wantedLens.category;
+    wantedLens.color = body.wantedLens.color;
+    wantedLens.function = body.wantedLens.function;
+    wantedLens.changeCycleRange = body.wantedLens.changeCycleRange;
+
+    const suitedLens = new SuitedLensDto();
+    suitedLens.brand = body.suitedLens.brand;
+    suitedLens.name = body.suitedLens.name;
+
+    findUser.wantedLens = wantedLens;
+    findUser.suitedLens = suitedLens;
+
+    await this.usersRepository.save(findUser);
   }
 }
