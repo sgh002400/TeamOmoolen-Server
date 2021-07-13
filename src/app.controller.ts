863cebfa1619819e47a6201a45e9f2d90a0d7a73
jihwan <sgh002400@naver.com>
2021-07-13 15:36:52 +0900

    [REFACTOR] - Response body format changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Query, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FilterConditionDto } from './common/dto/filter.condition.dto';
 import { saveOnBoardingDataDto } from './common/dto/save.onboarding.data.dto';
@@ -12,29 +12,48 @@ export class AppController {
 
   @Post('/saveOnBoardingData')
   async saveOnBoardingData(@Body() body: saveOnBoardingDataDto) {
-  
     const id = new ObjectId("60e7975bb8cadc72adc44f33");
     return this.appService.saveOnBoardingData(body, id);
   }
   
   @Get('/home')
-  async getHomeData() {
+  async getHomeData(@Res() res) {
     const id = new ObjectId("60e7975bb8cadc72adc44f33");
-    return this.appService.findHomeData(id);
+    const response = await this.appService.findHomeData(id);
+    res.status(200).send({
+      status: 200, 
+      message: "홈 화면 데이터 조회 완료",
+      data: response,
+    });
   }
   
   @Get('/searchWindow')
-  async getPopularItem() {
-    return this.appService.findPopularItem();
+  async getPopularItem(@Res() res) {
+    const response = await this.appService.findPopularItem();
+    res.status(200).send({
+      status: 200,
+      message: "인기 검색어 조회 성공",
+      data: response
+    })
   }
   
   @Get('/searchProduct')
-  async getSearchProduct(@Query() query: SearchQueryStringDto) {
-    return this.appService.getSearchProduct(query);
+  async getSearchProduct(@Query() query: SearchQueryStringDto, @Res() res) {
+    const response = await this.appService.getSearchProduct(query);
+    res.status(200).send({
+      status: 200,
+      message: "키워드 검색 성공",
+      data: response
+    })
   }
   
   @Get('/getFilteredList')
-  async filteredList(@Body() body: FilterConditionDto) {
-    return this.appService.getFilteredList(body);
+  async filteredList(@Body() body: FilterConditionDto, @Res() res) {
+    const response = await this.appService.getFilteredList(body);
+    res.status(200).send({
+      status: 200,
+      message: "필터 검색 성공",
+      data: response
+    })
   }
 }
